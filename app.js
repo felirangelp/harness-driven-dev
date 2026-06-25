bbae345e1cd8decce266d16f87c6a1222e2d86c5
Felipe Rangel <felirangelp@gmail.com>
2026-06-25 14:59:09 -0500

    fix: corregir filtro invertido en deleteTask
    Refs HAR-61

--- app.js
+++ app.js
@@ -155,7 +155,7 @@
   }
 
   function deleteTask(id) {
-    tasks = tasks.filter(function (t) { return t.id === id; });
+    tasks = tasks.filter(function (t) { return t.id !== id; });
     saveTasks(tasks);
     render();
   }
